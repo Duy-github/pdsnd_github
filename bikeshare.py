84abc1d8150b3d80b3b30effbc16a8217cde78bc
DuyND <duynd0307@outlook.com>
2024-04-03 22:47:16 +0700

    add: Simple math

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,12 @@ import time
 import pandas as pd
 import numpy as np
 
+
+#Simple math
+xx = 1
+yy = 2
+zz = xx + yy
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
